6daa683978c69e7eb3f2136148858fcf1a9acf0d
raheemelassadi <raheemelassadi1@gmail.com>
2023-04-08 03:12:46 -0700

    file init

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+
+document.querySelector('#app').innerHTML = `
+  <div class="grid h-screen place-items-center">
+    <header class="text-3xl font-bold underline">
+      <h1 class="text-2xl">Welcome to my project!</h1>
+    </header>
+  </div>
+`
+
