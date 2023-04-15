e449758193d2b8f94c0de431bc83061cc28da294
raheemelassadi <raheemelassadi1@gmail.com>
2023-04-15 04:16:16 -0700

    fixed navbar text color and changed nav bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      lightGrayBlue: 'hsl(220, 16%, 96%)',
+      blueGray: 'hsl(233, 8%, 62%)',
+      darkBlue: 'hsl(233, 26%, 24%)',
+    },
     extend: {},
   },
   plugins: [],
